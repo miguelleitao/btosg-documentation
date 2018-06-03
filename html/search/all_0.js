da0bc89cad39f34e6d4fc70da9d19c34e9615d7f
miguelleitao <jml@isep.ipp.pt>
2018-06-03 22:58:56 +0100

    documentation update

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['addobject',['addObject',['../classbtosgWorld.html#ae5b71c6319dd420479096a265a1725b7',1,'btosgWorld']]]
+  ['readme',['README',['../md_README.html',1,'']]]
 ];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,27 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "abcegilmnrsu",
-  1: "be",
-  2: "acglrsu",
-  3: "bimns",
-  4: "r"
+  0: "r",
+  1: "r"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "functions",
-  3: "variables",
-  4: "pages"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Functions",
-  3: "Variables",
-  4: "Pages"
+  1: "Pages"
 };
 
